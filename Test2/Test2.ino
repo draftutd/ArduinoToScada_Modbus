4beb0683e73af205394ad02c0f7a6603f4636746
Peerapong Pimup <draftutd@hotmail.com>
2019-05-13 21:27:31 +0700

    draftutd

--- Test2/Test2.ino
+++ Test2/Test2.ino
@@ -2,7 +2,9 @@
 #include <Ethernet.h>
 #include <Modbus.h>
 #include <ModbusIP.h>
-const int SENSOR_IREG = 100;
+const int SENSOR_IREG1 = 4000;
+const int SENSOR_IREG2 = 4001;
+const int SENSOR_IREG3 = 4002;
 const int sensorPin = A0;
 ModbusIP mb;
 long ts;
@@ -11,7 +13,9 @@ void setup() {
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   byte ip[] = { 192, 168, 137, 120 };
   mb.config(mac, ip);
-  mb.addIreg(SENSOR_IREG);
+  mb.addIreg(SENSOR_IREG1);
+  mb.addIreg(SENSOR_IREG2);
+  mb.addIreg(SENSOR_IREG3);
   ts = millis();
 }
 
@@ -19,6 +23,8 @@ void loop() {
   mb.task();
   if (millis() > ts + 1000) {
     ts = millis();
-    mb.Ireg(SENSOR_IREG, analogRead(sensorPin));
+    mb.Ireg(SENSOR_IREG1, 100);  //data.pm10_env
+    mb.Ireg(SENSOR_IREG2, 200);   //data.pm25_env
+    mb.Ireg(SENSOR_IREG3, 300); //data.pm100_env
   }
 }
